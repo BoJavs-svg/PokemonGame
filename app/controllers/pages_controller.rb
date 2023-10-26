0cf4d40473f6138238fe7f3260aaae9cdd121d03
BoJavs <javiereric1309@gmail.com>
2023-10-26 07:51:45 -0600

    Hints list works

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,9 @@
 class PagesController < ApplicationController
+  before_action :set_items
+  skip_before_action :verify_authenticity_token
+
+
   def inicio
-    @items = ["item 1", "item 2", "item 3", "item 4", "item 5", "item 6", "item 7", "item 8", "item 9", "item 10"]
   end
   # TODO fix button so that is shows correctly in the html file
   def update_hint
@@ -10,11 +13,27 @@ class PagesController < ApplicationController
   end
 
   def create_user
-    # Params {"authenticity_token"=>"dvYJQOaWaL1gzLmsXR8vRT7NMbzgnNWmxwP1vu1MyEs-b1SFtuXa8RfmIlT5iShsv0oluw50CTS26-H2GKcCQQ", "name"=>"javs", "commit"=>"Submit", "controller"=>"pages", "action"=>"create_user"}
     @user = User.new(name: params[:name],points: 0,hints: 0)
-    @user.save
+    if @user.save
+      p @user.id
+      redirect_to pages_main_path(id: @user.id)
+    end
   end 
 
+  def main
+    @user = User.find_by(id: params[:id])
+  end
 
+  def update_hint
+    @user = User.find_by(id: params[:id])
+    @user.hints += 1
+    @user.save
+    p @user.hints
+    render json: { hints: @items[0..@user.hints] }
+  end
+
+  def set_items
+    @items = ["item 1", "item 2", "item 3", "item 4", "item 5", "item 6", "item 7", "item 8", "item 9", "item 10"]
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   root 'pages#inicio'
-  
-  # No route matches [POST] "/pages/create_user"
+  # Main 
+  get 'pages/main', to: 'pages#main'
   post 'pages/create_user', to: 'pages#create_user'
+  post 'pages/update_hint', to: 'pages#update_hint'
   
-
-
 
 end
